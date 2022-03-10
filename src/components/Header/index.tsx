2d5826d6d587341887767737795f8d51ce8a5784
Lucas Cavalcante <lucas-cavalcante-2@hotmail.com>
2022-03-10 18:48:27 -0300

    Project Finalized

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+interface HeaderProps{
+  onOpenNewTransitionModal: () => void;
+}
+
+export function Header({onOpenNewTransitionModal}: HeaderProps) {
+  
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Logo Finance Control" />
+        <button type="button" onClick={onOpenNewTransitionModal}>
+          Nova Transação
+        </button>
+    
+        
+      </Content>
+    </Container>
+  );
+}
